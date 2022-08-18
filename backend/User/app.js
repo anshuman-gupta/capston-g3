846640b3d1f64cde712befe5c7b8dab7e3a33e20
Abhishek Patel <ap8209655@gmail.com>
2022-08-18 12:37:23 +0530

    Created User folder structure

--- backend/User/app.js
+++ backend/User/app.js
@@ -1,12 +1,14 @@
-const express = require("express")
-const server = express()
+const express = require("express");
+const server = express();
+const port = 3004;
+server.get("/", (req, res) => {
+  res.send("this is user page");
+});
 
-server.get("/",(req, res)=>{
-    res.send("this is user page")
-})
-
-server.get("/dashboard",(req, res)=>{
-    res.send("this is user dashboard")
-})
-
-module.exports = server
+server.get("/dashboard", (req, res) => {
+  res.send("this is user dashboard");
+});
+server.listen(port, (req, res) =>
+  console.log("server is running on port:", port)
+);
+module.exports = server;
