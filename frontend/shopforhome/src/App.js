26c983bc3364744b8f4b93a5931700e63b2fbc91
JojoChaitu <ramadietrance@gmail.com>
2022-08-18 20:19:33 +0530

    completed upto search functionality

--- frontend/shopforhome/src/App.js
+++ frontend/shopforhome/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter,Route,Routes} from 'react-router-dom'
+import  Home  from './components/Home';
+import SharedLayout from './components/SharedLayout'
+import Products from './components/Products';
+import About from './components/About';
+import SingleProduct from './components/SingleProduct';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Routes>
+              <Route path='/' element={<SharedLayout/>}>
+                <Route index element={<Home/>}></Route>
+                <Route path='about' element={<About/>}></Route>
+                <Route path='products' element={<Products/>}></Route>
+                <Route path='products/:productId' element={<SingleProduct/>}></Route>
+              </Route>
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/shopforhome/src/components/Navbar.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { CgProfile } from 'react-icons/cg'
+import { BsBookmarkHeart } from 'react-icons/bs'
+import { GrCart } from 'react-icons/gr'
+
+const  Navbar = () => {
+
+    // const navigate = useNavigate()
+    
+    return (
+        <div className="nav-container">
+            <div className="logo"><h2>NYKAA</h2></div>
+            <nav className='row'>
+                <div className="nav-items col">
+                    <p><NavLink to='/' className="NavLink">Home</NavLink></p>
+                    <p><NavLink to='about' className="NavLink">About</NavLink></p>
+                    <p><NavLink to='products' className="NavLink">Products</NavLink></p>
+                </div>
+                <ul className='col'>
+
+                    <li className="item">
+                        <BsBookmarkHeart className='icons'/>
+                    </li>
+                    <li className="item">
+                        <GrCart className='icons cart-icon'/>
+                    </li>
+                    <li className="item">
+                            <div class="dropdown">
+                            <div class=" dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <CgProfile className='icons'/>
+                            </div>
+
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Signup</a></li>
+                                <li><a class="dropdown-item" href="#">Login</a></li>
+                                <li><a class="dropdown-item" href="#">others</a></li>
+                            </ul>
+                            </div>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
